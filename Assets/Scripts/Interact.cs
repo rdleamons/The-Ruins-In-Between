2dda7efe5b77f0c29bfc7ccbd1c9d9725ab65392
Rebekah <rdleamons@gmail.com>
2022-01-29 02:28:52 -0600

    Working on talking to NPCs

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -7,50 +7,103 @@ public class Interact : MonoBehaviour
 {
     public GameObject textBox;
     private int index;
+    private int convoIndex;
     public GameObject textPopup;
 
-    private List<string> lines;
+    private List<string> introLines;
+    private List<string> nifraLines;
+    private List<string> adaLines;
+    private bool talkNifra;
+    private bool talkAda;
+    private bool readIntro;
 
     void Start()
     {
-        lines = new List<string>(File.ReadAllLines(Application.streamingAssetsPath + "/Intro.txt"));
+        readIntro = true;
+
+        introLines = new List<string>(File.ReadAllLines(Application.streamingAssetsPath + "/Intro.txt"));
+        nifraLines = new List<string>(File.ReadAllLines(Application.streamingAssetsPath + "/Nifra.txt"));
+        adaLines = new List<string>(File.ReadAllLines(Application.streamingAssetsPath + "/Ada.txt"));
+
         index = 0;
-        textBox.SetActive(true);
-        textBox.GetComponent<TextMeshProUGUI>().text = lines[0];
         textPopup.SetActive(false);
+        textBox.GetComponent<TextMeshProUGUI>().text = introLines[0];
     }
+
     private void Update()
     {
         if (textBox.GetComponent<TextMeshProUGUI>().text == " ")
-        {
             textBox.gameObject.SetActive(false);
-        }
         else
-        {
             textBox.gameObject.SetActive(true);
-        }
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (readIntro)
         {
-            index++;
-            textBox.GetComponent<TextMeshProUGUI>().text = lines[index];
+            if (Input.GetKeyDown(KeyCode.Return))
+                NextLine(introLines);
         }
 
-        if (index == lines.Count)
+
+        if(Input.GetKeyDown(KeyCode.E))
         {
-            textBox.GetComponent<TextMeshProUGUI>().text = " ";
+            if (talkNifra)
+            {
+                textPopup.SetActive(false);
+                textBox.GetComponent<TextMeshProUGUI>().text = nifraLines[index];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    NextLine(nifraLines);
+                }
+            }
+            else if (talkAda)
+            {
+                textPopup.SetActive(false);
+                textBox.GetComponent<TextMeshProUGUI>().text = adaLines[index];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    NextLine(adaLines);
+                }
+            }
         }
-
-        //if (index >= lines.Count)
-          //  index = lines.Count;
+    
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("NPC"))
         {
+            index = 0;
+            readIntro = false;
             textPopup.GetComponent<TextMeshPro>().text = "Press 'E' to talk."; 
             textPopup.SetActive(true);
+
+            if (other.gameObject.name == "Nifra")
+                talkNifra = true;
+            else if (other.gameObject.name == "Ada")
+                talkAda = true;
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            //talkNifra = false;
+            //talkAda = false;
+            textPopup.SetActive(false);
+        }
+    }
+
+    void NextLine(List<string> lines)
+    {
+        textBox.GetComponent<TextMeshProUGUI>().text = lines[index++];
+
+        if(index == lines.Count)
+        {
+            readIntro = false;
+            talkNifra = false;
+            talkAda = false;
+        }
+            
     }
 }
